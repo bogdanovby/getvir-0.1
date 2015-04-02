466851a958fa0fcf36132895dff9bd9302725e79
Pavel Bogdanov <pavel@bogdanov.by>
2015-04-02 07:00:01 +0300

    add signatures

--- getvir.sh
+++ getvir.sh
@@ -5,7 +5,6 @@
 for n in $1
 do
 
-echo -n "Linux distro "
 DISTRO="Unsupport"
 if [ -e /etc/redhat-release ]; then
         DISTRO="RHEL"
@@ -28,27 +27,25 @@ fi
 
 
 
- echo -e "$DISTRO"
- echo -e "$FIND_HOST"
-
-
 
 
 
 done
 
 
-#FIND_VIR1=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep -l "eval(base64_decod"`
-#FIND_VIR2=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep -l "preg_replace(\"\/"`
-#FIND_VIR3=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep "eval" | grep "_REQUEST" | awk -F ":" '{print $1}'`
+FIND_VIR1=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep -l "eval(base64_decod"`
+FIND_VIR2=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep -l "preg_replace(\"\/"`
+FIND_VIR3=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep "eval" | grep "_REQUEST" | awk -F ":" '{print $1}'`
+FIND_VIR4=`echo "$FIND_HOST" | xargs -i find "{}" -name '*.php' | xargs grep "eval"`
 
 
-#  echo -e "Hight Level: "
-#  echo -e "$FIND_VIR1"
-#  echo -e "$FIND_VIR3"
-#  echo -e " "
-#  echo -e "Medium Level: "
-#  echo -e "$FIND_VIR2"
+  echo -e "Hight Level: "
+  echo -e "$FIND_VIR1"
+  echo -e "$FIND_VIR3"
+  echo -e " "
+  echo -e "Medium Level: "
+  echo -e "$FIND_VIR2"
+  echo -e "$FIND_VIR4"
 
 #done
 
